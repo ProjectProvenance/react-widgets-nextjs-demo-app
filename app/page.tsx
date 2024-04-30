3d369ab3eb99c805c142edd7133398aea44ad0ae
Simone <hello@simonewebdesign.it>
2024-04-30 18:22:55 +0100

    Make it render server side again using search params

--- app/page.tsx
+++ app/page.tsx
@@ -1,41 +1,19 @@
-'use client'
-
 import Provenance from '@provenance/react-widgets'
 import PageContent from './page-content'
-// import { useState } from 'react'
-import { usePathname, useRouter, useSearchParams } from 'next/navigation';
+import SelectSku from './components/select-sku'
 
-const productIds = ['HS1', '1F2C']
-
-export default function Home() {
-  const { replace } = useRouter()
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
-  const handleChange = (newProductId: string) => {
-    const params = new URLSearchParams(searchParams)
-
-    params.set("productId", newProductId))
-
-    replace(`${pathname}?${params.toString()}`)
-  }
-
-  const productId = searchParams.get("productId")?.toString()
+export default function Home({
+  searchParams,
+}: {
+  searchParams?: {
+    productId?: string;
+  };
+}) {
+  const productId = searchParams?.productId || ""
 
   return (
     <main>
-      <label htmlFor="sku">
-        Select SKU:
-        <select
-          id="sku"
-          defaultValue="HS1"
-          onChange={e => handleChange(e.target.value)}
-        >
-          {productIds.map(id =>
-            <option key={id} value={id}>{id}</option>
-          )}
-        </select>
-      </label>
+      <SelectSku />
 
       <PageContent>
         <h1>This is a child of PageContent. Embeds below:</h1>
