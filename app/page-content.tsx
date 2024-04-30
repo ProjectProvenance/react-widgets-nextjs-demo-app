943ee64700f32bf63ee27c8165bb4c91f8b0d5d4
Simone <hello@simonewebdesign.it>
2024-04-30 18:17:01 +0100

    Getting there

--- app/page-content.tsx
+++ app/page-content.tsx
@@ -1,24 +1,8 @@
-"use client"
+'use client'
 
-import { useState } from 'react'
-
-export default function PageContent({ ids, children }) {
-  const [sku, setSku] = useState(ids[0])
-
+export default function PageContent({ children }) {
   return (
     <>
-      <label htmlFor="sku">
-        Select SKU:
-        <select
-          id="sku"
-          value={sku}
-          onChange={e => setSku(e.target.value)}
-        >
-          {ids.map(id =>
-            <option key={id} value={id}>{id}</option>
-          )}
-        </select>
-      </label>
       {children}
     </>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,46 @@
+'use client'
+
 import Provenance from '@provenance/react-widgets'
 import PageContent from './page-content'
+// import { useState } from 'react'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation';
 
 const productIds = ['HS1', '1F2C']
 
 export default function Home() {
+  const { replace } = useRouter()
+  const pathname = usePathname()
+  const searchParams = useSearchParams()
+
+  const handleChange = (newProductId: string) => {
+    const params = new URLSearchParams(searchParams)
+
+    params.set("productId", newProductId))
+
+    replace(`${pathname}?${params.toString()}`)
+  }
+
+  const productId = searchParams.get("productId")?.toString()
 
   return (
     <main>
-      <PageContent ids={productIds}>
-        <Provenance ids={productIds} />
+      <label htmlFor="sku">
+        Select SKU:
+        <select
+          id="sku"
+          defaultValue="HS1"
+          onChange={e => handleChange(e.target.value)}
+        >
+          {productIds.map(id =>
+            <option key={id} value={id}>{id}</option>
+          )}
+        </select>
+      </label>
+
+      <PageContent>
+        <h1>This is a child of PageContent. Embeds below:</h1>
+        { productId === 'HS1' && <Provenance.Embed uid="fb405L7e" productId="HS1" /> }
+        { productId === '1F2C' && <Provenance.Embed uid="fb405L7e" productId="1F2C" /> }
       </PageContent>
     </main>
   )
